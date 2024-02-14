5ddc7c853bb504cfa580c818809d9e6ee50bb971
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-14 05:02:10 +0000

    [Finish] 4-9

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # deviseの機能5つ
+  
+  has_many :post_images, dependent: :destroy
+  # 1:Nの関係を1側から示す, 1:Nの1側が削除されるとN側を全て削除
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users #devise を使用する際に URL として users を含む
   root to: 'homes#top'
+  devise_for :users #devise を使用する際に URL として users を含む
   
-  get 'homes/about', as: 'about'
+  resources :post_images, only: [:new, :index, :show]
+  # 不要なルーティングが実行されないようにonlyを設定
+  
+  get 'homes/about', as: 'about' #get "/homes/about" => "homes#about", as: "about"??
   # 名前付きルートをas:で設定
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
