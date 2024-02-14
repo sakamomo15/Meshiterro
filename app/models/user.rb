807162cb6249059746536d4c767a1f3d700384f3
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-14 09:28:33 +0000

    [Finish] 4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # deviseの機能5つ
-  
+    # deviseの機能5つ
+
   has_many :post_images, dependent: :destroy
-  # 1:Nの関係を1側から示す, 1:Nの1側が削除されるとN側を全て削除
+    # 1:Nの関係を1側から示す, 1:Nの1側が削除されるとN側を全て削除
+
+  has_one_attached :profile_image
+    # profile_imageという名前でActiveStorageでプロフィール画像を保存できるように設定
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+      # widthとheightは引数
+  end
+    # get_profile_image(100, 100) => 100x100の画像にリサイズ
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   # 不要なルーティングが実行されないようにonlyを設定
   
+  resources :users, only: [:show, :edit]
+  
   get 'homes/about', as: 'about' #get "/homes/about" => "homes#about", as: "about"??
   # 名前付きルートをas:で設定
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
