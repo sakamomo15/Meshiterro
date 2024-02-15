89830420213555be539df669131747eaed3cc318
sakamoto15 <155605617+sakamomo15@users.noreply.github.com>
2024-02-15 12:47:20 +0000

    [Update] 4-18

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,7 +3,7 @@ class PostCommentsController < ApplicationController
     post_image = PostImage.find(params[:post_image_id])
     comment = current_user.post_comments.new(post_comment_params)
     comment.post_image_id = post_image.id
-    comment.seve
+    comment.save
     redirect_to post_image_path(post_image)
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -56,14 +56,6 @@ ActiveRecord::Schema.define(version: 2024_02_14_152229) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "postimages", force: :cascade do |t|
-    t.string "shop_name"
-    t.text "caption"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
